5a05b0a17cfff51503070b5b8839cc74e0c439f6
Mariusz <koval09603@gmail.com>
2024-04-06 18:01:51 +0200

    zad2

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Point point = new Point(2.0, 3.0);
-        System.out.println("Wspolrzedne: (" + point.getX() + ", " + point.getY() + ")");
-    }
-}
